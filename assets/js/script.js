04ea8fa1395bcf82cbf8f2f28f73584eb980fb95
quicksilver524 <quicksilver524@gmail.com>
2022-05-27 19:44:53 -0400

    resized search img

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,17 +30,25 @@ function searchGame(event) {
       var gameImg = document.createElement("img");
       // add img element attribute src and gve it the content
       gameImg.setAttribute("src", data[0].thumb);
+      gameImg.setAttribute("style", "height: 100px");
 
       var infoCon = document.createElement("div");
       infoCon.setAttribute("class", "info-Con");
-      //infoCon.innerHTML = "";
+      infoCon.setAttribute("id", data[0].steamAppID);
+
+      var gameName = document.createElement("p");
+      gameName.setAttribute("class", "search-results");
+      gameName.innerHTML = data[0].external;
+      console.log(data[0].external);
 
       var cheapPrice = document.createElement("p");
       cheapPrice.setAttribute("class", "search-results");
       cheapPrice.innerHTML = "Cheapest Price:" + " $" + data[0].cheapest;
 
       // append children elements to the result container
-      resultContainer.append(gameImg, infoCon);
+      resultContainer.append(gameImg);
+      resultContainer.append(infoCon);
+      infoCon.appendChild(gameName);
       infoCon.append(cheapPrice);
     });
 }
