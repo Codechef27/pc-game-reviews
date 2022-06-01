b2c7f29fb69dded89cdd0f85233dda8abe167da8
quicksilver524 <quicksilver524@gmail.com>
2022-05-31 23:00:43 -0400

    merge conflicts with branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,9 @@
 //on form submit we will send the content to the form and search for a game
 var submitBtn = document.getElementById("game-search");
 var userInput = document.getElementById("user-input");
-var searchHistory = [];
-var searchHistoryEl = document.getElementById("search-history-list");
 
-function saveUserInput(search) {
-  // add search into seachhistory Array
-  searchHistory.push(search)
-  // store stringified array into local storage
-  localStorage.setItem('search-history', JSON.stringify(searchHistory));
-  // save search into local storage
-  // call function that creates buttons from data in local storage. 
-}
-
-function checkLocalStorage() {
-  // check local storage for a key called search history and save as variable called 'stored'
-  var stored = localStorage.getItem('search-history');
-
-  // if stored exists we want to parse stored and make it equal searchHistory
-  if (stored) {
-    searchHistory = JSON.parse(stored);
-    return true;
-  } else {
-    return false;
-  }
-
-  // call function that creates buttons from data in local storage. 
-
-}
-
-console.log(checkLocalStorage());
-
-function populateSearchHistory() {
-  if (checkLocalStorage()) {
-    for (let i = 0; i < searchHistory.length; i++) {
-      var listItem = document.createElement("li");
-      listItem.innerHTML = searchHistory[i];
-      searchHistoryEl.appendChild(listItem);
-    }
-  }
-}
-populateSearchHistory();
-// declare function that creates the search history buttons
-
-function captureUserInput(event) {
+function searchGame(event) {
   event.preventDefault();
-  searchGame(userInput.value)
-}
-
-function searchGame(search) {
-
 
   fetch(
     "https://www.cheapshark.com/api/1.0/games?title=" +
@@ -61,7 +15,7 @@ function searchGame(search) {
     })
     .then(function (data) {
       console.log(data);
-      saveUserInput(search);
+
       //clear seach bar
       userInput.value = "";
       //clear display search result
